d59b7ce0c046368351cc4d5f3d7c24f9b90a047e
Richard <rdobmk@gmail.com>
2024-12-07 01:08:50 +0100

    Challenge completed

---
+++ lib/models/groceries.dart
@@ -0,0 +1,11 @@
+class Grocery {
+  const Grocery({
+    required this.id,
+    required this.title,
+    required this.quantity,
+  });
+
+  final String id;
+  final String title;
+  final int quantity;
+}
