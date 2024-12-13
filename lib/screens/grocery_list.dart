a59ffeb506b0d1e43d5e3c13f00de20c5589c81e
Richard <rdobmk@gmail.com>
2024-12-13 18:29:19 +0100

    05 Managing the Loading State

--- lib/screens/grocery_list.dart
+++ lib/screens/grocery_list.dart
@@ -15,6 +15,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
 
   @override
   void initState() {
@@ -49,6 +50,7 @@ class _GroceryListState extends State<GroceryList> {
     }
     setState(() {
       _groceryItems = _loadedItems;
+      _isLoading = false;
     });
   }
 
@@ -98,6 +100,12 @@ class _GroceryListState extends State<GroceryList> {
         ],
       ),
     );
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
 
     if (_groceryItems.isNotEmpty) {
       content = ListView.builder(
